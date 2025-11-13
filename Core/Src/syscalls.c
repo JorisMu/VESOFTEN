eba02ba53060bb209d48ea2a43aa63abcfa844a5
JorisMu <jorismullink@gmail.com>
2025-11-13 11:53:08 +0100

    fix errors

---
+++ Core/Src/syscalls.c
@@ -0,0 +1,52 @@
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+#undef errno
+extern int errno;
+
+int _close(int file) {
+  return -1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+  return 0;
+}
+
+int _read(int file, char *ptr, int len) {
+  return 0;
+}
+
+int _write(int file, char *ptr, int len) {
+  return len;
+}
+
+int _fstat(int file, struct stat *st) {
+  st->st_mode = S_IFCHR;
+  return 0;
+}
+
+int _isatty(int file) {
+  return 1;
+}
+
+void _exit(int status) {
+  _write(1, "exit", 4);
+  while (1) {
+    ;
+  }
+}
+
+int _getpid(void) {
+  return 1;
+}
+
+int _kill(int pid, int sig) {
+  errno = EINVAL;
+  return -1;
+}
